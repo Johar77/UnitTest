248981beb8b26eaa342a80db95abfba805b10b4d
Johar77 <Johar77@163.com>
2017-01-01 23:24:41 +0800

    changes log

---
+++ UnitTest/UnitTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = Add(1, 2);
+            Console.WriteLine(c);
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ UnitTest/UnitTestTests/ProgramTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            int num1 = 1;
+            int num2 = 2;
+            int except = 3;
+            int real = Program.Add(num1, num2);
+            Assert.AreEqual(except, real);
+        }
+    }
+}
